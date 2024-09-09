34d5dfbbda5eb7598931eff37604243c24b00425
刘文锐 <liuwenrui597@qq.com>
2024-09-09 10:25:29 +0800

    chore: update license

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 export { validateChineseIDCard } from './validate/validateChineseIDCard';
 export { validateChinesePhoneNumber } from './validate/validateChinesePhoneNumber';
+export { validateChinesePhoneAndLandline } from './validate/validateChinesePhoneAndLandline';
 export { getUrlSearchParams } from './handler/getUrlSearchParams';
 export { getUrlQueryParams } from './handler/getUrlQueryParams';
